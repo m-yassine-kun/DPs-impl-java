77e1b6b254e816043aaf319b3b0ed54e9d87c90a
Koala <70296933+m-yassine-kun@users.noreply.github.com>
2022-07-03 14:16:27 +0100

    Update Main.java

--- DP_AbstractFactory/src/com/Main.java
+++ DP_AbstractFactory/src/com/Main.java
@@ -3,6 +3,9 @@ package com;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	FabriqueVehicule Fv;
+	Fv=new FabriqueVehiculeElecticité();
+	Automobile M1 = Fv.creeAutomobile("BMW","Bleu",120,5);
+	M1.afficheCaracteristiques();
     }
 }
